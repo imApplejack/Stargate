94b7aa0d8966cf5171d718367d79b9ad55b91d01
eric <eric.guilbaud@gmail.com>
2023-03-15 15:20:03 +0100

    client server

--- Main.cs
+++ Main.cs
@@ -22,6 +22,16 @@ public class Main : Node
      public PlayerControl Player1Vue;
      public PlayerControl Player2Vue;
 
+    [Export]
+    public string ip;
+
+    [Export]
+    public int port;
+
+    [Export]
+    public int seed;
+
+
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -29,10 +39,23 @@ public class Main : Node
 
 
         var host = new NetworkedMultiplayerENet();
-        host.CreateServer(6112, 10);
+
+        GD.Print(ip);
+        if (ip != String.Empty)
+        {
+            GD.Print("server");
+            host.CreateServer(port, 10);
+        }
+        else
+        {
+            GD.Print("client");
+            host.CreateClient(ip, port);
+        }
         GetTree().NetworkPeer = host;
-        GD.Print(GetTree().NetworkPeer);
         GD.Print(GetTree().IsNetworkServer());
+
+
+
 
         Library library = new Library(ProjectSettings.GlobalizePath("res://Sets/"));
         DeckImporter deckimporter = new DeckImporter(ProjectSettings.GlobalizePath("res://Sets/Decks/"));
@@ -58,9 +81,11 @@ public class Main : Node
         Player2Vue.InitControl(game.GameState.player2, game.GameState.player1, mappingMVC2, this);
         game.GameState.StargateResultHandler += Player2Vue.HandleResult;
 
-        game.GameState.InitGame(1);
+        game.GameState.InitGame(seed);
 
 
+
+        GD.PrintErr(ip);
 
        // ((WindowDialog)FindNode("WindowDialog")).PopupCentered();
         
